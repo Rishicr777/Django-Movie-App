7c92b4b8e49606488b6ae4b4e070545a96dcf78e
Rishi <rishikanwar77@gmail.com>
2019-09-19 18:56:04 +0530

    Pushing to heroku

--- movies/views.py
+++ movies/views.py
@@ -3,10 +3,12 @@ from django.contrib import messages
 from airtable import Airtable
 import os
 
+AIRTABLE_MOVIESTABLE_BASE_ID="appSvHgpElzjhjmNk"
+AIRTABLE_API_KEY="keyYVIuveejFWqwTt"
 
-AT = Airtable(os.environ.get('AIRTABLE_MOVIESTABLE_BASE_ID'),
+AT = Airtable(os.environ.get('AIRTABLE_MOVIESTABLE_BASE_ID',AIRTABLE_MOVIESTABLE_BASE_ID),
               'Movies',
-              api_key=os.environ.get('AIRTABLE_API_KEY'))
+              api_key=os.environ.get('AIRTABLE_API_KEY', AIRTABLE_API_KEY))
 
 # Create your views here.
 def home_page(request):
